07f27312e7dedd9d732f690791b75e3ff16e48a9
Mostafa Ismail <mostafaismaiel10@gmail.com>
2023-03-06 10:54:52 +0200

    updates
    Fix prayers api
    Enhance some css code

--- js/main.js
+++ js/main.js
@@ -1,208 +1,208 @@
 $(document).ready(function () {
-    // Calculation ramadan count down
-    (function () {
-        let remainingTimeElement = $(".container-remainingTime"),
-            todayGeorgianDate = moment(),
-            todayHijiriFormat = todayGeorgianDate.format("iYYYY/iM/iD"),
-            todayHijiriDate = moment(todayHijiriFormat, "iYYYY/iM/iD"),
-            thisHijiriYear = parseInt(todayGeorgianDate.format("iYYYY")),
-            ramadanDate = moment(`${thisHijiriYear}/9/1`, "iYYYY/iM/iD"),
-            daysInRamadan = ramadanDate.daysInMonth(),
-            diff = todayHijiriDate.diff(ramadanDate, "days");
+  // Calculation ramadan count down
+  (function () {
+    let remainingTimeElement = $(".container-remainingTime"),
+      todayGeorgianDate = moment(),
+      todayHijiriFormat = todayGeorgianDate.format("iYYYY/iM/iD"),
+      todayHijiriDate = moment(todayHijiriFormat, "iYYYY/iM/iD"),
+      thisHijiriYear = parseInt(todayGeorgianDate.format("iYYYY")),
+      ramadanDate = moment(`${thisHijiriYear}/9/1`, "iYYYY/iM/iD"),
+      daysInRamadan = ramadanDate.daysInMonth(),
+      diff = todayHijiriDate.diff(ramadanDate, "days");
 
-        if (diff >= 0) {
-            if (diff <= daysInRamadan) {
-                $(".container-text").addClass('container--duringRamdan');
-                diff = daysInRamadan - diff;
-            } else {
-                let nextHijiriYear = thisHijiriYear + 1;
+    if (diff >= 0) {
+      if (diff <= daysInRamadan) {
+        $(".container-text").addClass("container--duringRamdan");
+        diff = daysInRamadan - diff;
+      } else {
+        let nextHijiriYear = thisHijiriYear + 1;
 
-                ramadanDate = moment(`${nextHijiriYear}/9/1`, "iYYYY/iM/iD");
-                diff = todayHijiriDate.diff(ramadanDate, "days");
-            }
-        }
+        ramadanDate = moment(`${nextHijiriYear}/9/1`, "iYYYY/iM/iD");
+        diff = todayHijiriDate.diff(ramadanDate, "days");
+      }
+    }
 
-        remainingTimeElement.text(Math.abs(diff).toLocaleString("ar-sa"));
-    })();
+    remainingTimeElement.text(Math.abs(diff).toLocaleString("ar-sa"));
+  })();
 
-    // variables
-    let city = $('#city-list'),
-        prayList = $("#pray-list"),
-        imsak = $('#imsak'),
-        imsakOverLay = $('#over-lay'),
-        imsakClose = $('#imsak-close'),
-        imsakBtn = $('#imsak-btn'),
-        morningAzkar = $('#morning-azkar'),
-        morningAzkarOverlay = $('#morning-azkar-overlay'),
-        morningAzkarClose = $('#morning-azkar-close'),
-        morningAzkarBtn = $('#morning-azkar-btn'),
-        morningAzkarList = $('#morning-azkar-list'),
-        eveningAzkar = $('#evening-azkar'),
-        eveningAzkarOverlay = $('#evening-azkar-overlay'),
-        eveningAzkarClose = $('#evening-azkar-close'),
-        eveningAzkarBtn = $('#evening-azkar-btn'),
-        eveningAzkarList = $('#evening-azkar-list');
+  // variables
+  let city = $("#city-list"),
+    prayList = $("#pray-list"),
+    imsak = $("#imsak"),
+    imsakOverLay = $("#over-lay"),
+    imsakClose = $("#imsak-close"),
+    imsakBtn = $("#imsak-btn"),
+    morningAzkar = $("#morning-azkar"),
+    morningAzkarOverlay = $("#morning-azkar-overlay"),
+    morningAzkarClose = $("#morning-azkar-close"),
+    morningAzkarBtn = $("#morning-azkar-btn"),
+    morningAzkarList = $("#morning-azkar-list"),
+    eveningAzkar = $("#evening-azkar"),
+    eveningAzkarOverlay = $("#evening-azkar-overlay"),
+    eveningAzkarClose = $("#evening-azkar-close"),
+    eveningAzkarBtn = $("#evening-azkar-btn"),
+    eveningAzkarList = $("#evening-azkar-list");
 
+  // Fetch prayer times
+  function getPrayerTime() {
+    const today = new Date().toISOString().slice(0, 10);
 
-
-    // Fetch prayer times
-    function getPrayerTime() {
-        const today = new Date().toISOString().slice(0, 10);
-
-        $.ajax({
-            method: "GET",
-            url: `https://api.pray.zone/v2/times/day.json?city=${city.val()}&date=${today}&school=2&timeformat=1`,
-            success: function (response, status, request) {
-                const times = response.results.datetime[0].times;
-                prayList.html("");
-                for (let [key, value] of Object.entries(times)) {
-                    if (key == 'Sunrise' || key == 'Sunset' || key == 'Midnight') {
-                        continue;
-                    }
-                    $(`<li class='pray-info'>
+    $.ajax({
+      method: "GET",
+      url: `https://api.aladhan.com/v1/timingsByCity/${today}?city=${city.val()}&country=egypt&method=2`,
+      success: function (response) {
+        const times = response.data.timings;
+        prayList.html("");
+        prayList.css({ opacity: "1", visibility: "visible" });
+        for (let [key, value] of Object.entries(times)) {
+          if (
+            [
+              "sunrise",
+              "sunset",
+              "midnight",
+              "imsak",
+              "lastthird",
+              "firstthird",
+            ].includes(key.toLowerCase())
+          ) {
+            continue;
+          }
+          $(`<li class='pray-info'>
                 <p class='pray-name'>${key}</p>
                 <p class='pray-time'>${value}</p>
                 </li>`).appendTo(prayList);
-                }
-            },
-            error: function (request, status, error) {
-                console.log(request);
-                console.log(status);
-                console.log(error);
-            },
-        });
-    };
+        }
+      },
+      error: function (request, status, error) {
+        console.log(request);
+        console.log(status);
+        console.log(error);
+      },
+    });
+  }
 
-    // Fetch morning azkar
-    function getMorningAzkar() {
-        $.ajax({
-            method: "GET",
-            url: "json/azkar.json",
-            success: function (response, status, request) {
-                for (let i = 0; i <= response.length - 1; i += 1) {
-                    if (response[i].category !== 'أذكار الصباح') {
-                        continue;
-                    }
-                    // console.log(response[i]);
-                    $(`<li class="azkar-info">
+  // Fetch morning azkar
+  function getMorningAzkar() {
+    $.ajax({
+      method: "GET",
+      url: "json/azkar.json",
+      success: function (response, status, request) {
+        for (let i = 0; i <= response.length - 1; i += 1) {
+          if (response[i].category !== "أذكار الصباح") {
+            continue;
+          }
+          // console.log(response[i]);
+          $(`<li class="azkar-info">
                 <p class='azkar-name'>${response[i].zekr}</p>
                 <p class='azkar-count'>Count: " ${response[i].count} "</p>
                 </li>`).appendTo(morningAzkarList);
-                }
-            }
-        });
-    };
-    getMorningAzkar();
+        }
+      },
+    });
+  }
+  getMorningAzkar();
 
-    // Fetch evening azkar
-    function getEveningAzkar() {
-        $.ajax({
-            method: "GET",
-            url: "json/azkar.json",
-            success: function (response, status, request) {
-                for (let i = 0; i <= response.length - 1; i += 1) {
-                    if (response[i].category !== 'أذكار المساء') {
-                        continue;
-                    }
-                    $(`<li class="azkar-info">
+  // Fetch evening azkar
+  function getEveningAzkar() {
+    $.ajax({
+      method: "GET",
+      url: "json/azkar.json",
+      success: function (response, status, request) {
+        for (let i = 0; i <= response.length - 1; i += 1) {
+          if (response[i].category !== "أذكار المساء") {
+            continue;
+          }
+          $(`<li class="azkar-info">
                 <p class='azkar-name'>${response[i].zekr}</p>
                 <p class='azkar-count'>Count: " ${response[i].count} "</p>
                 </li>`).appendTo(eveningAzkarList);
-                }
-            }
-        });
-    };
-    getEveningAzkar();
+        }
+      },
+    });
+  }
+  getEveningAzkar();
 
+  // Add event listner
 
-    // Add event listner
-
-    city.on('change', function () {
-        prayList.html(`<div class="spinner-border" role="status">
+  city.on("change", function () {
+    prayList.html(`<div class="spinner-border" role="status">
   <span class="sr-only">Loading...</span>
 </div>`);
-        getPrayerTime();
+    getPrayerTime();
+  });
+
+  // Fade out/in ramdan imsak
+  imsakBtn.on("click", function () {
+    imsak.css({
+      opacity: "1",
+      visibility: "visible",
     });
+    $("html").css("overflow", "hidden");
+  });
 
-    // Fade out/in ramdan imsak
-    imsakBtn.on('click', function () {
-        imsak.css({
-            opacity: "1",
-            visibility: 'visible'
-        });
-        $('html').css('overflow', 'hidden');
+  imsakClose.on("click", function () {
+    imsak.css({
+      opacity: "0",
+      visibility: "hidden",
     });
+    $("html").css("overflow", "unset");
+  });
 
-    imsakClose.on('click', function () {
-        imsak.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
+  imsakOverLay.on("click", function () {
+    imsak.css({
+      opacity: "0",
+      visibility: "hidden",
     });
+    $("html").css("overflow", "unset");
+  });
 
-    imsakOverLay.on('click', function () {
-        imsak.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
+  // Fade out/in morning azkar
+  morningAzkarBtn.on("click", function () {
+    morningAzkar.css({
+      opacity: "1",
+      visibility: "visible",
     });
+    $("html").css("overflow", "hidden");
+  });
 
-
-    // Fade out/in morning azkar
-    morningAzkarBtn.on('click', function () {
-        morningAzkar.css({
-            opacity: "1",
-            visibility: 'visible'
-        });
-        $('html').css('overflow', 'hidden');
+  morningAzkarClose.on("click", function () {
+    morningAzkar.css({
+      opacity: "0",
+      visibility: "hidden",
     });
+    $("html").css("overflow", "unset");
+  });
 
-    morningAzkarClose.on('click', function () {
-        morningAzkar.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
+  morningAzkarOverlay.on("click", function () {
+    morningAzkar.css({
+      opacity: "0",
+      visibility: "hidden",
     });
+    $("html").css("overflow", "unset");
+  });
 
-    morningAzkarOverlay.on('click', function () {
-        morningAzkar.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
+  // Fade out/in evening azkar
+  eveningAzkarBtn.on("click", function () {
+    eveningAzkar.css({
+      opacity: "1",
+      visibility: "visible",
     });
+    $("html").css("overflow", "hidden");
+  });
 
-    // Fade out/in evening azkar
-    eveningAzkarBtn.on('click', function () {
-        eveningAzkar.css({
-            opacity: "1",
-            visibility: 'visible'
-        });
-        $('html').css('overflow', 'hidden');
+  eveningAzkarClose.on("click", function () {
+    eveningAzkar.css({
+      opacity: "0",
+      visibility: "hidden",
     });
+    $("html").css("overflow", "unset");
+  });
 
-    eveningAzkarClose.on('click', function () {
-        eveningAzkar.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
+  eveningAzkarOverlay.on("click", function () {
+    eveningAzkar.css({
+      opacity: "0",
+      visibility: "hidden",
     });
-
-    eveningAzkarOverlay.on('click', function () {
-        eveningAzkar.css({
-            opacity: "0",
-            visibility: 'hidden'
-        });
-        $('html').css('overflow', 'unset');
-
-    });
+    $("html").css("overflow", "unset");
+  });
 });
